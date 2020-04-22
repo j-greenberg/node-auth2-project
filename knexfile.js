6283e1c035fa6e496f3302b6695a946edaecf87b
j-greenberg <jgreenberg@runbox.com>
2020-04-22 17:23:53 -0400

    updated

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,9 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/auth.db3'
     }
   },
 
@@ -17,11 +18,18 @@ module.exports = {
       password: 'password'
     },
     pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
     }
   },
 
